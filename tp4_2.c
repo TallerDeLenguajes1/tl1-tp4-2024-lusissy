a62fe155f8e1ce5537397ffce27fa564bab1f6e1
Lourdes <lourdessissy@gmail.com>
2025-04-15 00:41:48 -0300

    tp4_2_1

--- tp4_2.c
+++ tp4_2.c
@@ -4,18 +4,64 @@
 
 #define MAX_DESC 100
 
-// Estructura para una Tarea
-typedef struct {
+ struct {
     int TareaID;
     char *Descripcion;
-    int Duracion; // En minutos (10-100)
-} Tarea;
+    int Duracion;
+} typedef Tarea;
 
-// Nodo para la lista enlazada
-typedef struct Nodo {
+struct Nodo {
     Tarea T;
     struct Nodo *Siguiente;
-} Nodo;
-int main(){
+} typedef Nodo;
+
+Nodo* crearNodo(int id, const char *desc, int duracion) {
+    Nodo *nuevo = (Nodo*)malloc(sizeof(Nodo));
+    nuevo->T.TareaID = id;
+    nuevo->T.Descripcion = (char*)malloc(strlen(desc) + 1);
+    strcpy(nuevo->T.Descripcion, desc);
+    nuevo->T.Duracion = duracion;
+    nuevo->Siguiente = NULL;
+    return nuevo;
+}
+
+void insertarTarea(Nodo **lista, Nodo *nueva) {
+    nueva->Siguiente = *lista;
+    *lista = nueva;
+}
+
+void mostrarTareas(Nodo *lista) {
+    printf("\n--- TAREAS PENDIENTES ---\n");
+    while (lista != NULL) {
+        printf("ID: %d | Desc: %s | Duración: %d mins\n",
+               lista->T.TareaID, lista->T.Descripcion, lista->T.Duracion);
+        lista = lista->Siguiente;
+    }
+}
+
+int main() {
+    Nodo *pendientes = NULL;
+    int tareaID = 1000;
+    int continuar;
+    char desc[MAX_DESC];
+    int duracion;
+
+    do {
+        printf("\nIngrese descripción de la tarea: ");
+        gets(desc);
+        
+
+        printf("Ingrese duración (10-100): ");
+        scanf("%d", &duracion);
+
+        Nodo *nueva = crearNodo(tareaID++, desc, duracion);
+        insertarTarea(&pendientes, nueva);
+
+        printf("¿Desea agregar otra tarea? (1: Sí / 0: No): ");
+        scanf("%d", &continuar);
+
+    } while (continuar != 0);
+
+    mostrarTareas(pendientes);
     return 0;
 }
